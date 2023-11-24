ee090eb64d7258ad28a308efb4a612b404d97a00
Martin <nmabregu@gmail.com>
2023-11-24 10:48:28 -0300

    first commit

---
+++ database/seeders/CategorySeeder.ts
@@ -0,0 +1,8 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Category from 'App/Models/Category'
+
+export default class CategorySeeder extends BaseSeeder {
+  public async run() {
+    await Category.createMany([{ name: 'Home' }, { name: 'Electronic' }, { name: 'Sports' }])
+  }
+}

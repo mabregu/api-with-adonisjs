ce462ad3968b05ee53fdd075cdee0ecc8dee0119
Martin <nmabregu@gmail.com>
2023-11-24 13:10:51 -0300

    implementando login de usuario

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -3,7 +3,7 @@ import User from 'App/Models/User'
 import RegisterValidator from 'App/Validators/RegisterValidator'
 
 export default class AuthController {
-  public async register({ request, auth, response }: HttpContextContract) {
+  public async register({ request, auth }: HttpContextContract) {
     const validatedData = await request.validate(RegisterValidator)
 
     const user = await User.create(validatedData)
@@ -11,5 +11,17 @@ export default class AuthController {
     const token = await auth.login(user)
 
     return token
+  }
+
+  public async login({ request, auth }: HttpContextContract) {
+    const { email, password } = request.all()
+
+    try {
+      const token = await auth.attempt(email, password)
+
+      return token
+    } catch (error) {
+      return "We couldn't verify your credentials."
+    }
   }
 }
